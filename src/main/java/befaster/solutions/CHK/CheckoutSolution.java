2850a7bc4bd5846e1b18c4a0510d84285c7451a2
sbx_user1051 <sbx_user1051@169.254.80.13>
2019-09-01 22:13:15 +0000

    Sun Sep 01 22:13:15 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,31 +1,34 @@
 package befaster.solutions.CHK;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class CheckoutSolution {
 
-    private Map<String, Integer> prices = new HashMap(){{
+    private Map<Character, Integer> prices = new HashMap(){{
         put("A", 50);
         put("B", 30);
         put("C", 20);
         put("D", 15);
     }} ;
 
-    private Map<String, Offer> offers = new HashMap(){{
+    private Map<Character, Offer> offers = new HashMap(){{
         put("A", new Offer(3, 130));
         put("B", new Offer(2, 45));
     }};
 
     public Integer checkout(String skus) {
-        Map<Character, Integer> orderedSkus= skus.chars().boxed()
-                .collect(Collectors.toMap(
-                        k -> (char) k.intValue(),
-                        v -> 1,
-                        Integer::sum))
-                );
+        Map<Character, Integer> orderedSkus = skus.chars()
+                .mapToObj(i -> (char))
+                .collect(Collectors.toMap(k -> ((char) k.intValue()), v -> 1, Integer::sum));
+
+        for (Character c : orderedSkus.keySet()) {
+
+            if (!prices.containsKey(c)) {
+                return -1;
+            }
+        }
     }
 
     private class Offer {
@@ -46,6 +49,7 @@ public class CheckoutSolution {
         }
     }
 }
+
 
 
 
