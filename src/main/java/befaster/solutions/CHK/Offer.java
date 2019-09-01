bffd0eef58da65c1361dbf131b336bdeb2765592
sbx_user1051 <sbx_user1051@169.254.80.13>
2019-09-01 22:38:50 +0000

    Sun Sep 01 22:38:50 UTC 2019

---
+++ src/main/java/befaster/solutions/CHK/Offer.java
@@ -0,0 +1,14 @@
+package befaster.solutions.CHK;
+
+public class Offer {
+
+    private Integer quantity;
+
+    public Offer(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+}
