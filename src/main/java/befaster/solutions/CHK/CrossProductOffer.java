e6f806e2efc57372ff0315131b4c66f5d243fe0c
sbx_user1051 <sbx_user1051@169.254.8.245>
2019-09-01 22:57:01 +0000

    Sun Sep 01 22:57:01 UTC 2019

---
+++ src/main/java/befaster/solutions/CHK/CrossProductOffer.java
@@ -0,0 +1,14 @@
+package befaster.solutions.CHK;
+
+public abstract class Offer {
+
+    private Integer quantity;
+
+    public Offer(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+}
