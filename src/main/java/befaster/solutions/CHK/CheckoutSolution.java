c011ad3b56deee2006464f63909732b46d2857b7
sbx_user1051 <sbx_user1051@169.254.80.13>
2019-09-01 21:58:15 +0000

    Sun Sep 01 21:58:15 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,7 +3,10 @@ package befaster.solutions.CHK;
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
+
+    private Map<>
     public Integer checkout(String skus) {
         throw new SolutionNotImplementedException();
     }
 }
+
